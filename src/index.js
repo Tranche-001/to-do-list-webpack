4bb8da09e0d17f8cca812ac50f6db5f8aa4af8cb
Tranche-001 <tranche100@gmail.com>
2025-08-14 20:30:17 -0300

    remove comments from index.js

--- src/index.js
+++ src/index.js
@@ -5,27 +5,7 @@ import { TodoList } from "./todoList";
 const newTodoItem = new TodoItem("a", "b", "c", "d", true);
 const newTodoList = new TodoList([newTodoItem]);
 domManipulator.showTodoList(newTodoList);
-// const content = document.querySelector(".todo-list");
 
-// content.innerHTML = `
-//  <table>
-      // <tr>
-      //   <th>Title</th>
-      //   <th>Description</th>
-      //   <th>Due-Date</th>
-      //   <th>Priority</th>
-      //   <th>Completed</th>
-      // </tr>
-//       <tr>
-//         <td>a</td>
-//         <td>b</td>
-//         <td>c</td>
-//         <td>d</td>
-//         <td><input type="checkbox"></td>
-//       </tr>
-//       <tr></tr>
-//     </table>
-//     `;
   
 
 
